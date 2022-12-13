1bb18b3f78f2dce8046ada3822ff0679baa14b55
siddhant <ssiddhant3030@gmail.com>
2022-12-13 15:01:27 +0530

    push all

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://github.com',
-  baseUrl: '/siddhant3030/my-website.git',
+  baseUrl: '/my-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -17,7 +17,12 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'siddhant3030', // Usually your GitHub org/user name.
-  projectName: 'my-website', // Usually your repo name.
+  projectName: 'my-website', 
+  deploymentBranch: 'gh-pages',
+  plugins: [[
+    require.resolve('@cmfcmf/docusaurus-search-local'), {indexBlog: false},
+  ]],
+  
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
